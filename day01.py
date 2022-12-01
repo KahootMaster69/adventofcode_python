cf7095c6a5773dac45462cd16a41a7b1a059546f
KahootMaster69 <jonas.foerster.2003@gmail.com>
2022-12-01 17:11:35 +0100

    day1 part2

--- day01.py
+++ day01.py
@@ -1,16 +1,18 @@
 def main():
     with open("input_01") as file:
         current_sum = 0
-        max_sum = 0
+        max_sums = [0, 0, 0]
         for line in file.readlines():
             line = line[:-1]
             if not line:
-                max_sum = max(current_sum, max_sum)
+                if current_sum > max_sums[0]:
+                    max_sums[0] = current_sum
+                    max_sums.sort()
                 current_sum = 0
             else:
                 current_sum += int(line)
 
-    print(max_sum)
+    print(sum(max_sums))
 
 
 if __name__ == '__main__':
